7d2d651c52e0fd269cc025a3201035e9b6b4189c
yersonh <solanoalfonsoy@gmail.com>
2025-12-05 09:44:46 -0500

    update

--- index.php
+++ index.php
@@ -417,7 +417,7 @@ function procesarRecuperacion($db, $correoUsuario, $base_url) {
     <link rel="icon" href="/imagenes/logo.png" type="image/png">
     <link rel="shortcut icon" href="/imagenes/logo.png" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="css/login-style.css">
+    <link rel="stylesheet" href="views/styles/login-style.css">
 </head>
 <body>
     <div class="container">
