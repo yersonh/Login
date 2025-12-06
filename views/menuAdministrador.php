98a25cf72cd44b1a34d72bb062bef784b14d46b8
yersonh <solanoalfonsoy@gmail.com>
2025-12-06 18:34:18 -0500

    update

--- views/menuAdministrador.php
+++ views/menuAdministrador.php
@@ -41,7 +41,7 @@ $correoUsuario = $_SESSION['correo'] ?? '';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel Administrativo - Secretaría de Minas y Energía</title>
     <link rel="icon" href="../../imagenes/logo.png" type="image/png">
-    <link rel="shortcut icon" href="../../imagenes/logo.png" type="image/png">
+    <link rel="shortcut icon" href="/../imagenes/logo.png" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- Enlace al CSS modularizado -->
     <link rel="stylesheet" href="styles/admin.css">
