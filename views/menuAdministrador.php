931716b5ef8d1b13b3af35ac9bf17d103ab0193a
yersonh <solanoalfonsoy@gmail.com>
2025-12-06 18:28:10 -0500

    update

--- views/menuAdministrador.php
+++ views/menuAdministrador.php
@@ -11,7 +11,7 @@
     <link rel="shortcut icon" href="../../imagenes/logo.png" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- Enlace al CSS modularizado -->
-    <link rel="stylesheet" href="../styles/admin.css">
+    <link rel="stylesheet" href="/styles/admin.css">
 </head>
 <body>
     <div class="admin-container">
