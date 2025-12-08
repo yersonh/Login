9d014409e0a96ac9625c82ab7b8fb674eea46140
yersonh <solanoalfonsoy@gmail.com>
2025-12-08 15:06:11 -0500

    update

--- index.php
+++ index.php
@@ -580,10 +580,6 @@ function generarEmailRecuperacion($nombrePersona, $link, $logo_url) {
                     </div>
                 <?php endif; ?>
 
-                <div class="signup">
-                    ¿No tienes cuenta? <a href="<?php echo $base_url; ?>/views/registrarusuario.php">Regístrate</a>
-                </div>
-
                 <div class="sisgonTech">
                     <small>Desarrollado por SisgonTech 2026. Version de prueba Runtime</small>
                 </div>
