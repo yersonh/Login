7f051dc504ace5c00f880c9dbdf7b7a23ef4b1b7
yersonh <solanoalfonsoy@gmail.com>
2025-12-08 07:56:55 -0500

    update

--- views/menuAdministrador.php
+++ views/menuAdministrador.php
@@ -138,7 +138,7 @@ $correoUsuario = $_SESSION['correo'] ?? '';
         <aside class="sidebar">
             <!-- Logo Institucional -->
             <div class="logo-section">
-               <!--  <img src="../imagenes/logo.png" alt="Logo Gobernación del Meta" class="admin-logo">-->
+               <img src="../imagenes/logo.png" alt="Logo Gobernación del Meta" class="footer-logo">
                 <div class="department-name">Gestión del Sistena</div>
                <!-- <div class="department-subtitle">Secretaría de Minas y Energía</div>-->
             </div>
