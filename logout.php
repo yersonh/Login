57c1485b2e43c93a5329bb0811a1df99ad23b07c
yersonh <solanoalfonsoy@gmail.com>
2025-12-08 08:40:02 -0500

    update

--- logout.php
+++ logout.php
@@ -1,16 +1,13 @@
 <?php
-// logout.php
+
 session_start();
 
-// Registrar el logout
 if (isset($_SESSION['correo'])) {
     error_log("Logout de usuario: " . $_SESSION['correo']);
 }
 
-// Destruir todas las variables de sesión
 $_SESSION = array();
 
-// Destruir la cookie de sesión
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -19,13 +16,10 @@ if (ini_get("session.use_cookies")) {
     );
 }
 
-// Destruir la sesión
 session_destroy();
 
-// Destruir cookie remember_token
 setcookie('remember_token', '', time() - 3600, '/');
 
-// Redirigir al login
-header("Location: index.php");
+header("Location: /index.php");
 exit();
 ?>
