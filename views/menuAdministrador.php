6168ec2bd9a3ec36387a8540c8667751f24a93a2
yersonh <solanoalfonsoy@gmail.com>
2025-12-09 10:23:51 -0500

    update

--- views/menuAdministrador.php
+++ views/menuAdministrador.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
 if (!isset($_SESSION['usuario_id'])) {
     header("Location: ../index.php");
