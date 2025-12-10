771b172ca6f73102cf87402f751173f6157c77f2
yersonh <solanoalfonsoy@gmail.com>
2025-12-10 13:39:34 -0500

    update

--- views/CPS/OpcionesCPS.php
+++ views/CPS/OpcionesCPS.php
@@ -8,7 +8,7 @@
     <link rel="shortcut icon" href="/imagenes/logo.png" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- Enlace al archivo CSS modularizado -->
-    <link rel="stylesheet" href="/../../styles/asistente.css">
+    <link rel="stylesheet" href="/styles/asistente.css">
 
 </head>
 <body>
