3f5259b507cfc1bfcc6041fe826068485a5f7cf9
yersonh <solanoalfonsoy@gmail.com>
2025-12-10 16:59:13 -0500

    update

--- views/CPS/menuContratistas.php
+++ views/CPS/menuContratistas.php
@@ -166,37 +166,6 @@ if (empty($nombreCompleto)) {
         </footer>
     </div>
     
-    <!-- MODAL PARA INGRESAR CLAVE -->
-    <div class="modal-overlay" id="modalClave">
-        <div class="modal-clave">
-            <div class="modal-header">
-                <h3>Acceso restringido</h3>
-                <p>Verificación de seguridad requerida</p>
-            </div>
-            <div class="modal-body">
-                <p>Ingrese la clave autorizada para parametrizar:</p>
-                <div class="input-group">
-                    <label for="inputClave">Clave de autorización</label>
-                    <div class="clave-container">
-                        <input type="password" id="inputClave" class="clave-input" placeholder="Digite la clave..." maxlength="20" autocomplete="off">
-                        <button type="button" class="clave-eye" id="togglePassword">
-                            <i class="fas fa-eye"></i>
-                        </button>
-                    </div>
-                </div>
-                <div class="error-message" id="errorMessage"></div>
-                <div class="modal-buttons">
-                    <button class="btn-modal btn-ingresar" id="btnIngresarClave">
-                        Ingresar
-                    </button>
-                    <button class="btn-modal btn-cancelar" id="btnCancelarClave">
-                        Cancelar
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
-    
     <!-- Incluye BASE_URL si es necesario para JavaScript -->
     <script>
         const USER_CORREO = "<?php echo $_SESSION['correo'] ?? 'No identificado'; ?>";
