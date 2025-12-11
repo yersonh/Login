5dfcbbf6d941fcb47afd8c59ba75b35e4d1214b1
yersonh <solanoalfonsoy@gmail.com>
2025-12-10 20:59:08 -0500

    update

--- api/health-check.php
+++ api/health-check.php
@@ -1,7 +1,4 @@
 <?php
-// Esto es suficiente para un healthcheck básico.
-// Simplemente devuelve un código de estado 200 OK
-// y un cuerpo mínimo.
 http_response_code(200);
-echo "OK";
+echo json_encode(["status" => "ok"]);
 ?>
