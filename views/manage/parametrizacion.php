97cee4ee0c498e9e0fc1e7181b6dce7c80e3f226
yersonh <solanoalfonsoy@gmail.com>
2025-12-10 22:47:31 -0500

    update

--- views/manage/parametrizacion.php
+++ views/manage/parametrizacion.php
@@ -41,7 +41,7 @@ $correoUsuario = $_SESSION['correo'] ?? '';
     <link rel="icon" href="/imagenes/logo.png" type="image/png">
     <link rel="shortcut icon" href="/imagenes/logo.png" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="../styles/parametrizacion.css">
+    <link rel="stylesheet" href="../styles/parametrizar.css">
 </head>
 <body>
     <div class="app-container">
