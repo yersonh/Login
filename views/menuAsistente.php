d6db9beed39a729149a31d56c13344e1145df01c
yersonh <solanoalfonsoy@gmail.com>
2025-12-13 17:38:53 -0500

    sirve

--- views/menuAsistente.php
+++ views/menuAsistente.php
@@ -224,7 +224,7 @@ $base_url = $protocol . "://" . $_SERVER['HTTP_HOST'];
         <!-- Segunda línea concatenada -->
         <p>
             <?php echo $direccion; ?> - Asesores e-Governance Solutions para Entidades Públicas <?php echo $anio; ?>® 
-            By: Ing. Rubén Darío González García <?php echo $telefono; ?>. Reservados todos los derechos de autor. - Contacto: <strong><?php echo $correo; ?></strong>
+            By: Ing. Rubén Darío González García <?php echo $telefono; ?>. Contacto: <strong><?php echo $correo; ?></strong> - Reservados todos los derechos de autor.  
         </p>
         
 
