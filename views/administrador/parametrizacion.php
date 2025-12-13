cb6b225a3c52ec68651f83f4310472f7f295cfa6
yersonh <solanoalfonsoy@gmail.com>
2025-12-13 17:52:32 -0500

    sirve

--- views/administrador/parametrizacion.php
+++ views/administrador/parametrizacion.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+require_once __DIR__ . '/../../helpers/config_helper.php';
 // Solo administradores
 if (!isset($_SESSION['usuario_id'])) {
     header("Location: ../index.php");
