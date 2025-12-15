f218ea9ca3bfb1a8ceb9ea2bcd7e7b5b1fdf537f
yersonh <solanoalfonsoy@gmail.com>
2025-12-15 00:29:24 -0500

    sirve

--- views/CPS/visor_registrados.php
+++ views/CPS/visor_registrados.php
@@ -47,7 +47,7 @@ try {
     <link rel="icon" href="/imagenes/logo.png" type="image/png">
     <link rel="shortcut icon" href="/imagenes/logo.png" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="../styles/visor_registrados.css">
+    <link rel="stylesheet" href="../styles/asistente.css">
 </head>
 <body>
     
