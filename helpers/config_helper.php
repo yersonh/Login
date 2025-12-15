e387e3f27b781a9da9cfb2a63236f73859cc4d29
yersonh <solanoalfonsoy@gmail.com>
2025-12-15 16:28:18 -0500

    sirve

--- helpers/config_helper.php
+++ helpers/config_helper.php
@@ -23,7 +23,7 @@ class ConfigHelper {
                     'ruta_logo' => 'imagenes/logo.png',        // ← SIN / al inicio
                     'entidad' => 'Gobernación del Meta',
                     'enlace_web' => 'https://www.meta.gov.co',
-                    'NIT' => '892000148-8'
+                    'nit' => '892000148-8'
                 ];
             }
         }
