28a8210e9a029769674bd446494b776b55eea789
yersonh <solanoalfonsoy@gmail.com>
2025-12-22 17:37:11 -0500

    sirve

--- views/CPS/SitiosAsignados.php
+++ views/CPS/SitiosAsignados.php
@@ -1,7 +1,3 @@
-<?php
-// SitiosAsignados.php - Mapa OSM centrado en el Meta
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -9,27 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sitios Asignados - Meta</title>
     
-    <!-- Leaflet -->
     <link rel="shortcut icon" href="/imagenes/logo.png" type="image/png">
+
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
-    <link rel="stylesheet" href="../styles/sitios_asignados.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
     
+    <link rel="stylesheet" href="../styles/sitios_asignados.css">
 </head>
 <body>
-    <!-- Solo el mapa -->
-    <div id="mapa"></div>
-    
-    <!-- Barra informativa -->
+
     <div class="barra-superior">
-        🗺️ Departamento del Meta - Colombia
+        <i class="fas fa-map-marked-alt"></i> Departamento del Meta - Colombia
     </div>
+
+    <div id="mapa" style="height: 100vh; width: 100%;"></div>
+
     <button class="volver-btn" id="volverBtn">
-            <i class="fas fa-arrow-left"></i>
-            <span>Volver al Menú</span>
+        <i class="fas fa-arrow-left"></i>
+        <span>Volver al Menú</span>
     </button>
+
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
     
+    <script>
+        document.getElementById('volverBtn').addEventListener('click', () => {
+            window.location.href = 'OpcionesCPS.php';
+        });
+    </script>
+
     <script src="../../javascript/sitios_asignados.js"></script>
 
 </body>
