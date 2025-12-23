d9990949f6759eb1afe99f8b39620a8cf07de5ca
yersonh <solanoalfonsoy@gmail.com>
2025-12-23 14:57:58 -0500

    sirve

--- views/menuAsistente.php
+++ views/menuAsistente.php
@@ -3,9 +3,11 @@ session_start();
 
 require_once __DIR__ . '/../helpers/config_helper.php';
 
-header("Cache-Control: no-cache, no-store, must-revalidate"); 
-header("Pragma: no-cache"); 
-header("Expires: 0");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); 
 
 if (!isset($_SESSION['usuario_id'])) {
     header("Location: ../index.php");
