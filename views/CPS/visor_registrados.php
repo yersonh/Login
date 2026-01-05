29273be173caf19f5a7ad799c29bde9a7c1360b3
yersonh <solanoalfonsoy@gmail.com>
2026-01-05 09:26:13 -0500

    sirve

--- views/CPS/visor_registrados.php
+++ views/CPS/visor_registrados.php
@@ -94,9 +94,6 @@ try {
     <link rel="shortcut icon" href="/imagenes/logo.png" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="../styles/visor_registrados.css">
-    <style>
-        
-    </style>
 </head>
 <body>
     
